e6fd38f533833a70a17488317086ac3e3a147e5d
g16941ss <g16941ss@s30420.tsuda.ac.jp>
2018-04-19 13:36:57 +0900

    文字列表示のプログラム

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("猫欲しいなり");
+		System.out.println("睡眠時間6時間が丁度いいと最近気づいた。");
 		// TODO Auto-generated method stub
 
 	}
